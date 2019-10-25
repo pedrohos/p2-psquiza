00b640ab0440a380576b39efbafbb6ca929f63c0
Pedro Henrique <lairpedroph@gmail.com>
2019-10-24 21:48:38 -0300

    Adicionei o EasyAccept e os casos de teste.
    O Easyaccept foi adicionado chamado e os casos de teste também.

--- src/psquiza/Psquiza.java
+++ src/psquiza/Psquiza.java
@@ -1,10 +1,19 @@
 package psquiza;
 
+import easyaccept.EasyAccept;
+
 public class Psquiza {
 
 	private Sistema sistema;
 	
 	public Psquiza() {
 		sistema = new Sistema();
+	}
+	
+	public static void main(String[] args) {
+		args = new String[] {"psquiza.Psquiza", "testes_aceitacao/use_case_1.txt", "testes_aceitacao/use_case_2.txt",
+							 "testes_aceitacao/use_case_3.txt", "testes_aceitacao/use_case_4.txt"};
+		
+		EasyAccept.main(args);
 	}
 }
