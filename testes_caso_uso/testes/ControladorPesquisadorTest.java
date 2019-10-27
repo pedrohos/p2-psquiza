4fc15e1ee3b32a6eb33f119caae9dfef6c313260
reginaLeticia <regina.felipe@ccc.ufcg.edu.br>
2019-10-27 20:54:32 -0300

    Junit

---
+++ testes_caso_uso/testes/ControladorPesquisadorTest.java
@@ -0,0 +1,44 @@
+package testes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ControladorPesquisadorTest {
+
+	@Test
+	void testControladorPesquisador() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCadastraPesquisador() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAlteraPesquisador() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDesativaPesquisador() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAtivaPesquisador() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testExibePesquisador() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPesquisadorEhAtivo() {
+		fail("Not yet implemented");
+	}
+
+}
