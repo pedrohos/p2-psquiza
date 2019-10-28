9d3edbdbed656018cf7cd191d35d804ecb99a1db
Lucianjfc <48529800+Lucianjfc@users.noreply.github.com>
2019-10-28 11:08:21 -0300

    Criado os testes da US4
    criados os testes da us4

---
+++ testes_caso_uso/testes_use_case4/TestAtividade.java
@@ -0,0 +1,106 @@
+package testes_use_case4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import psquiza.Atividade;
+
+public class TestAtividade {
+
+	@Test
+	public void criaAtividade() {
+		Atividade ati = new Atividade("aaa", "BAIXO", "fui", "A1");
+	}
+
+	@Test
+	public void criaAtividadeDescricaoVazia() {
+		try {
+			Atividade ati = new Atividade("", "BAIXO", "fui", "A1");
+			assertFalse(false);
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+	}
+
+	@Test
+	public void criaAtividadeDescricaoRiscoVazio() {
+		try {
+			Atividade ati = new Atividade("aaa", "BAIXO", "", "A1");
+			assertFalse(false);
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+	}
+
+	@Test
+	public void criaAtividadeRiscoBaixo() {
+		Atividade ati = new Atividade("aaa", "BAIXO", "fui", "A1");
+	}
+
+	@Test
+	public void criaAtividadeRiscoMedio() {
+		Atividade ati = new Atividade("aaa", "MEDIO", "fui", "A1");
+	}
+
+	@Test
+	public void criaAtividadeRiscoAlto() {
+		Atividade ati = new Atividade("aaa", "ALTO", "fui", "A1");
+	}
+
+	@Test
+	public void criaAtividadeRiscoDiferente() {
+		try {
+			Atividade ati = new Atividade("aaa", "alto demais", "fui", "A1");
+			assertFalse(false);
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+	}
+
+	@Test
+	public void cadastraItem() {
+		Atividade ati = new Atividade("aaa", "BAIXO", "fui", "A1");
+		ati.cadastraItem("janela");
+	}
+
+	@Test
+	public void cadastraItemVazio() {
+		try {
+			Atividade ati = new Atividade("aaa", "BAIXO", "fui", "A1");
+			ati.cadastraItem("");
+			assertFalse(false);
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+	}
+
+	@Test
+	public void pegaItensPendentes() {
+		Atividade ati = new Atividade("aaa", "BAIXO", "fui", "A1");
+		ati.cadastraItem("janela");
+		ati.cadastraItem("porta");
+		ati.cadastraItem("celeiro");
+		ati.cadastraItem("cachaca");
+		assertEquals(4, ati.getItensPendentes());
+	}
+
+	@Test
+	public void pegaItensRealizados() {
+		Atividade ati = new Atividade("aaa", "BAIXO", "fui", "A1");
+		ati.cadastraItem("janela");
+		ati.cadastraItem("porta");
+		ati.cadastraItem("celeiro");
+		ati.cadastraItem("cachaca");
+		assertEquals(0, ati.getItensRealizados());
+	}
+
+	@Test
+	public void toStringAtividade() {
+		Atividade ati = new Atividade("Monitoramento de chats dos alunos de computacao do primeiro periodo.", "BAIXO",
+				"Por se tratar de apenas um monitoramento, o risco nao e elevado.", "A1");
+		assertEquals(
+				"Monitoramento de chats dos alunos de computacao do primeiro periodo. (BAIXO - Por se tratar de apenas um monitoramento, o risco nao e elevado.)",
+				ati.toString());
+	}
+}
---
+++ testes_caso_uso/testes_use_case4/TestControladorAtividade.java
@@ -0,0 +1,233 @@
+package testes_use_case4;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.Test;
+
+import psquiza.ControladorAtividade;
+
+public class TestControladorAtividade {
+
+	@Test
+	public void TestCadastraAtividadeRiscoBaixo() {
+		ControladorAtividade c = new ControladorAtividade();
+		c.cadastraAtividade("Monitoramento de chats dos alunos de computacao do primeiro periodo.", "BAIXO",
+				"Por se tratar de apenas um monitoramento, o risco nao e elevado.");
+	}
+
+	@Test
+	public void TestCadastraAtividadeRiscoMedio() {
+		ControladorAtividade c = new ControladorAtividade();
+		c.cadastraAtividade("Monitoramento de chats dos alunos de computacao do primeiro periodo.", "MEDIO",
+				"Por se tratar de apenas um monitoramento, o risco nao e elevado.");
+	}
+
+	@Test
+	public void TestCadastraAtividadeRiscoAlto() {
+		ControladorAtividade c = new ControladorAtividade();
+		c.cadastraAtividade("Monitoramento de chats dos alunos de computacao do primeiro periodo.", "ALTO",
+				"Por se tratar de apenas um monitoramento, o risco nao e elevado.");
+	}
+
+	@Test
+	public void TestCadastraAtividadeDescricaoNull() {
+		ControladorAtividade c = new ControladorAtividade();
+		try {
+			c.cadastraAtividade("", "BAIXO", "mais ou menos");
+			assertFalse(false);
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+	}
+
+	@Test
+	public void TestCadastraAtividadeRiscoNull() {
+		ControladorAtividade c = new ControladorAtividade();
+		try {
+			c.cadastraAtividade("bom", "", "mais ou menos");
+			assertFalse(false);
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+	}
+
+	@Test
+	public void TestCadastraAtividadeRiscoDescricaoRisco() {
+		ControladorAtividade c = new ControladorAtividade();
+		try {
+			c.cadastraAtividade("bom", "BAIXO", "");
+			assertFalse(false);
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+	}
+
+	@Test
+	public void TestCadastraAtividadeRiscoDesconhecido() {
+		ControladorAtividade c = new ControladorAtividade();
+		try {
+			c.cadastraAtividade("bom", "MUITOALTO", "desas");
+			assertFalse(false);
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+	}
+
+	@Test
+	public void TestApagaAtividade() {
+		ControladorAtividade c = new ControladorAtividade();
+		c.cadastraAtividade("bom", "BAIXO", "desas");
+		c.apagaAtividade("A1");
+	}
+
+	@Test
+	public void TestApagaAtividadeNaoCadastrada() {
+		ControladorAtividade c = new ControladorAtividade();
+		try {
+			c.cadastraAtividade("bom", "BAIXO", "desas");
+			c.apagaAtividade("A1");
+			assertFalse(false);
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+	}
+
+	@Test
+	public void cadastraItem() {
+		ControladorAtividade c = new ControladorAtividade();
+		c.cadastraAtividade("bom", "BAIXO", "desas");
+		c.cadastraItem("A1", "Monitoramento facebook/messenger");
+	}
+
+	@Test
+	public void cadastraItemIdNull() {
+		ControladorAtividade c = new ControladorAtividade();
+		try {
+			c.cadastraAtividade("bom", "BAIXO", "desas");
+			c.cadastraItem("", "Monitoramento facebook/messenger");
+			assertFalse(false);
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+	}
+
+	@Test
+	public void cadastraItemVazio() {
+		ControladorAtividade c = new ControladorAtividade();
+		try {
+			c.cadastraAtividade("bom", "BAIXO", "desas");
+			c.cadastraItem("A1", "");
+			assertFalse(false);
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+	}
+
+	@Test
+	public void exibeAtividade() {
+		ControladorAtividade c = new ControladorAtividade();
+		c.cadastraAtividade("Monitoramento de chats dos alunos de computacao do primeiro periodo.", "BAIXO",
+				"Por se tratar de apenas um monitoramento, o risco nao e elevado.");
+		assertEquals(
+				"Monitoramento de chats dos alunos de computacao do primeiro periodo. (BAIXO - Por se tratar de apenas um monitoramento, o risco nao e elevado.)",
+				c.exibeAtividade("A1"));
+	}
+
+	@Test
+	public void exibeAtividadeIdVazio() {
+		try {
+			ControladorAtividade c = new ControladorAtividade();
+			c.cadastraAtividade("Monitoramento de chats dos alunos de computacao do primeiro periodo.", "BAIXO",
+					"Por se tratar de apenas um monitoramento, o risco nao e elevado.");
+			c.exibeAtividade("");
+			assertFalse(false);
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+	}
+
+	@Test
+	public void contaItensPendentes() {
+		ControladorAtividade c = new ControladorAtividade();
+		c.cadastraAtividade("Monitoramento de chats dos alunos de computacao do primeiro periodo.", "BAIXO",
+				"Por se tratar de apenas um monitoramento, o risco nao e elevado.");
+		c.cadastraItem("A1", "tijolo");
+		c.cadastraItem("A1", "Bloco de pedra");
+		c.cadastraItem("A1", "reator nuclear");
+		c.cadastraItem("A1", "frigideira");
+		assertEquals(4, c.contaItensPendentes("A1"));
+	}
+
+	@Test
+	public void contaItensPendentes2() {
+		ControladorAtividade c = new ControladorAtividade();
+		c.cadastraAtividade("Monitoramento de chats dos alunos de computacao do primeiro periodo.", "BAIXO",
+				"Por se tratar de apenas um monitoramento, o risco nao e elevado.");
+		c.cadastraItem("A1", "tijolo");
+		c.cadastraItem("A1", "Bloco de pedra");
+		c.cadastraItem("A1", "reator nuclear");
+		c.cadastraItem("A1", "frigideira");
+		assertEquals(4, c.contaItensPendentes("A1"));
+		c.cadastraItem("A1", "cerveja");
+		c.cadastraItem("A1", "carne");
+		assertEquals(6, c.contaItensPendentes("A1"));
+	}
+
+	@Test
+	public void ContaItensPendentesIdVazio() {
+		try {
+			ControladorAtividade c = new ControladorAtividade();
+			c.cadastraAtividade("Monitoramento de chats dos alunos de computacao do primeiro periodo.", "BAIXO",
+					"Por se tratar de apenas um monitoramento, o risco nao e elevado.");
+			c.cadastraItem("A1", "tijolo");
+			c.cadastraItem("A1", "Bloco de pedra");
+			c.cadastraItem("A1", "reator nuclear");
+			c.cadastraItem("A1", "frigideira");
+			assertEquals(4, c.contaItensPendentes("A1"));
+			c.cadastraItem("A1", "cerveja");
+			c.cadastraItem("A1", "carne");
+			assertEquals(6, c.contaItensPendentes(""));
+			assertFalse(false);
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+	}
+
+	@Test
+	public void contaItensRealizados() {
+		ControladorAtividade c = new ControladorAtividade();
+		c.cadastraAtividade("Monitoramento de chats dos alunos de computacao do primeiro periodo.", "BAIXO",
+				"Por se tratar de apenas um monitoramento, o risco nao e elevado.");
+		c.cadastraItem("A1", "tijolo");
+		c.cadastraItem("A1", "Bloco de pedra");
+		c.cadastraItem("A1", "reator nuclear");
+		c.cadastraItem("A1", "frigideira");
+		assertEquals(4, c.contaItensPendentes("A1"));
+		c.cadastraItem("A1", "cerveja");
+		c.cadastraItem("A1", "carne");
+		assertEquals(0, c.contaItensRealizados("A1"));
+	}
+
+	@Test
+	public void ContaItensRealizadosIdVazio() {
+		try {
+			ControladorAtividade c = new ControladorAtividade();
+			c.cadastraAtividade("Monitoramento de chats dos alunos de computacao do primeiro periodo.", "BAIXO",
+					"Por se tratar de apenas um monitoramento, o risco nao e elevado.");
+			c.cadastraItem("A1", "tijolo");
+			c.cadastraItem("A1", "Bloco de pedra");
+			c.cadastraItem("A1", "reator nuclear");
+			c.cadastraItem("A1", "frigideira");
+			assertEquals(4, c.contaItensPendentes("A1"));
+			c.cadastraItem("A1", "cerveja");
+			c.cadastraItem("A1", "carne");
+			assertEquals(0, c.contaItensRealizados(""));
+			assertFalse(false);
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+	}
+
+}
