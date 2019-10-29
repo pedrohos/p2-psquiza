e4a54902b0d14239a81cfa4c5379dea5229066c9
Pedro Henrique <pedro.henrique.silva@ccc.ufcg.edu.br>
2019-10-29 07:28:47 -0300

    Documentei o US4 e modifiquei alguns erros semanticos.
    Documentei as classes:
    ATIVIDADE, CONTROLADORATIVIDADE, ESTADO, ITEM, RISCO.
    E modifiquei pequenos detalhes no código.

--- src/psquiza/Atividade.java
+++ src/psquiza/Atividade.java
@@ -3,17 +3,79 @@ package psquiza;
 import java.time.Period;
 import java.util.LinkedHashSet;
 
+/**
+ * Representacao de uma Atividade no sistema.
+ * Cada Atividade apresenta uma descricao, um nivel
+ * de risco, uma descricao do risco, uma duracao (periodo),
+ * um id que o identifica unicamente em seu controlador e
+ * uma lista de itens que compoem a Atividade.
+ * 
+ * O nivel de risco deve ser um dos 3:
+ * BAIXO
+ * MEDIO
+ * ALTO
+ * 
+ * E possivel alem de criar uma Atividade, cadastrar um item,
+ * retornar a quantidade de itens no status de PENDENTE e de
+ * REALIZADO.
+ * 
+ * @author Pedro Henrique
+ */
 public class Atividade {
+	
+	/**
+	 * Armazena a descricao da Atividade.
+	 */
 	private String descricao;
+	
+	/**
+	 * Armazena a descricao do risco da Atividade.
+	 */
 	private String descricaoRisco;
+	
+	/**
+	 * Armazena o nivel de risco da Atividade, podem estar em um
+	 * de 3 estados:
+	 * BAIXO, MEDIO ou ALTO
+	 */
 	private Risco nivelRisco;
+	
+	/**
+	 * Representa a duracao da Atividade.
+	 */
 	private Period periodo;
+	
+	/**
+	 * Representa o identificador unico da Atividade.
+	 */
 	private String id;
+	
+	/**
+	 * Armazena um conjunto ordenado de itens que compoem a atividade.
+	 */
 	private LinkedHashSet<Item> itens;
 
+	/**
+	 * Constroi um nova atividade, validando se os parametros passados
+	 * sao vazios ou nulos, caso sejam sera lancado um IllegalArgumentException:
+	 * 
+	 * Descricao vazia ou nulo: "Campo Descricao nao pode ser nulo ou vazio."
+	 * Nivel de Risco vazio ou nulo: "Campo nivelRisco nao pode ser nulo ou vazio."
+	 * ID vazio ou nulo: "Campo codigo nao pode ser nulo ou vazio."
+	 * Descricao do Risco vazia ou nula: "Campo descricaoRisco nao pode ser nulo ou vazio."
+	 * 
+	 * Caso o Nivle de Risco nao seja BAIXO, MEDIO ou ALTO, sera lancado um
+	 * IllegalArgumentException: "Valor invalido do nivel do risco."
+	 * 
+	 * @param descricao e a descricao da atividade.
+	 * @param nivelRisco e o nivel de risco da ativdade.
+	 * @param descricaoRisco e a descricao do risco da atividade.
+	 * @param id e o identificador unico da atividade.
+	 */
 	public Atividade(String descricao, String nivelRisco, String descricaoRisco, String id) {
 		Util.validaAtributo(descricao, "Campo Descricao nao pode ser nulo ou vazio.");
 		Util.validaAtributo(nivelRisco, "Campo nivelRisco nao pode ser nulo ou vazio.");
+		Util.validaAtributo(id, "Campo codigo nao pode ser nulo ou vazio.");
 		switch (nivelRisco) {
 		case "BAIXO":
 			this.nivelRisco = Risco.BAIXO;
@@ -35,12 +97,25 @@ public class Atividade {
 		this.itens = new LinkedHashSet<>();
 	}
 
+	/**
+	 * Cadastra um item no conjunto ordenado de itens da Atividade.
+	 * 
+	 * Caso o item seja vazio ou nulo sera lancado um IllegalArgumentException:
+	 * "Item nao pode ser nulo ou vazio."
+	 * 
+	 * @param item e o item que sera adicionado.
+	 */
 	public void cadastraItem(String item) {
 		Util.validaAtributo(item, "Item nao pode ser nulo ou vazio.");
 
 		this.itens.add(new Item(item));
 	}
 
+	/**
+	 * Retorna a quantidade de itens com o status de PENDENTE.
+	 * 
+	 * @return e retornado a quantidade de itens PENDENTE.
+	 */
 	public int getItensPendentes() {
 		int resultado = 0;
 		for (Item item : itens) {
@@ -51,6 +126,11 @@ public class Atividade {
 		return resultado;
 	}
 
+	/**
+	 * Retorna a quantidade de itens com o status de REALIZADO.
+	 * 
+	 * @return e retornado a quantidade de itens REALIZADO.
+	 */
 	public int getItensRealizados() {
 		int resultado = 0;
 		for (Item item : itens) {
@@ -73,6 +153,12 @@ public class Atividade {
 		return this.descricaoRisco;
 	}
 
+	/**
+	 * Retorna a representacao toString de Atividade no formato:
+	 * DESCRICAO (NIVELRISCO - DESCRICAORISCO)
+	 * 
+	 * @return e retornado a representacao toString de Atividade.
+	 */
 	@Override
 	public String toString() {
 		return String.format("%s (%s - %s)", getDescricao(), getNivelRisco(), getDescricaoRisco());
--- src/psquiza/ControladorAtividade.java
+++ src/psquiza/ControladorAtividade.java
@@ -2,11 +2,43 @@ package psquiza;
 
 import java.util.HashMap;
 
+/**
+ * Representacao do gerenciador de Atividades no sistema.
+ * 
+ * Possui um ID gerador e um ID processado.
+ * Possui um mapa de atividades que sao cadastradas e recuperadas
+ * pelo ID: "'A' + IDPROCESSADO"
+ * E possivel cadastrar, exibir e apagar uma atividade.
+ * E possivel cadastrar e exibir itens de uma atividade, bem
+ * contar os itens pendentes e os realizados.
+ * 
+ * @author Pedro Henrique
+ */
 public class ControladorAtividade {
+	
+	/**
+	 * Armazena um mapa que mapeia um id a uma Atividade.
+	 */
 	private HashMap<String, Atividade> atividades;
+	
+	/**
+	 * Representa o id final da atividade.
+	 */
 	private String idAtual;
+	
+	/**
+	 * Representa o id gerador do id da atividade.
+	 */
 	private int codigoId;
 
+	/**
+	 * Verifica se existe uma atividade a partir de seu codigo.
+	 * Se a atividade existir no mapa de atividades e retornado true,
+	 * caso contrario false.
+	 * 
+	 * @param codigo e o id que identifica a atividade.
+	 * @return e retornado um boolean indicando se  atividade existe ou nao.
+	 */
 	private boolean existeAtividade(String codigo) {
 		if (this.atividades.containsKey(codigo)) {
 			return true;
@@ -14,18 +46,49 @@ public class ControladorAtividade {
 		return false;
 	}
 
+	/**
+	 * Constroi o controlador criando um novo mapa de atividades,
+	 * definindo o id gerador como 1 inicialmente e armazenando um
+	 * ID final.
+	 */
 	public ControladorAtividade() {
 		this.atividades = new HashMap<>();
 		this.codigoId = 1;
 		this.idAtual = "A" + codigoId;
 	}
 
+	/**
+	 * Cadastra uma atividade no mapa de atividades.
+	 * Cada atividade deve ser cadastrada com uma descricao, nivel de risco
+	 * e descricao do risco.
+	 * 
+	 * O nivel de risco pode (deve) ser:
+	 * BAIXO
+	 * MEDIO
+	 * ALTO
+	 * 
+	 * O id e mapeado como chave da atividade e um novo e gerado.
+	 * 
+	 * @param descricao e a descricao da atividade.
+	 * @param nivelRisco e o nivel de risco da atividade.
+	 * @param descricaoRisco e a descricao do risco da atividade.
+	 */
 	public void cadastraAtividade(String descricao, String nivelRisco, String descricaoRisco) {
 		this.atividades.put(this.idAtual, new Atividade(descricao, nivelRisco, descricaoRisco, this.idAtual));
 		this.codigoId++;
 		this.idAtual = "A" + codigoId;
 	}
 
+	/**
+	 * Exclui uma atividade a partir de seu id.
+	 * 
+	 * Caso o id seja nulo ou vazio sera lancado um IllegalArgumentException:
+	 * "Campo codigo nao pode ser nulo ou vazio."
+	 * Caso o id nao remeta a nenhuma atividade sera lancado um IllegalArgumentException:
+	 * "Atividade nao encontrada"
+	 * 
+	 * @param id e o id que identifica a atividade.
+	 */
 	public void apagaAtividade(String id) {
 		Util.validaAtributo(id, "Campo codigo nao pode ser nulo ou vazio.");
 		if (!existeAtividade(id))
@@ -34,6 +97,20 @@ public class ControladorAtividade {
 		this.atividades.remove(id);
 	}
 
+	/**
+	 * Cadastra um item em uma atividade.
+	 * O item e composto por uma String.
+	 * 
+	 * Caso o id seja nulo ou vazio sera lancado um IllegalArgumentException:
+	 * "Campo codigo nao pode ser nulo ou vazio."
+	 * Caso o item seja nulo ou vazio sera lancado um IllegalArgumentException:
+	 * "Item nao pode ser nulo ou vazio."
+	 * Caso o id nao remeta a nenhuma atividade sera lancado um IllegalArgumentException:
+	 * "Atividade nao encontrada"
+	 * 
+	 * @param id e o id que remete a uma atividade.
+	 * @param item e a descricao do Item.
+	 */
 	public void cadastraItem(String id, String item) {
 		Util.validaAtributo(id, "Campo codigo nao pode ser nulo ou vazio.");
 		Util.validaAtributo(item, "Item nao pode ser nulo ou vazio.");
@@ -43,6 +120,18 @@ public class ControladorAtividade {
 		this.atividades.get(id).cadastraItem(item);
 	}
 
+	/**
+	 * Exibe a representacao String de atividade, que segue o padrao:
+	 * DESCRICAO (NIVELRISCO - DESCRICAORISCO)
+	 * 
+	 * Caso o id seja nulo ou vazio sera lancado um IllegalArgumentException:
+	 * "Campo codigo nao pode ser nulo ou vazio."
+	 * Caso o id nao remeta a nenhuma atividade sera lancado um IllegalArgumentException:
+	 * "Atividade nao encontrada"
+	 * 
+	 * @param id e o id que remete a uma atividade.
+	 * @return retorna a representacao toString de dada atividade.
+	 */
 	public String exibeAtividade(String id) {
 		Util.validaAtributo(id, "Campo codigo nao pode ser nulo ou vazio.");
 		if (!existeAtividade(id))
@@ -51,6 +140,18 @@ public class ControladorAtividade {
 		return this.atividades.get(id).toString();
 	}
 
+	/**
+	 * Retorna a quantidade itens que possui o status de PENDENTE de
+	 * determinada atividade.
+	 * 
+	 * Caso o id nao remeta a nenhuma atividade sera lancado um IllegalArgumentException:
+	 * "Atividade nao encontrada"
+	 * Caso o id seja nulo ou vazio sera lancado um IllegalArgumentException:
+	 * "Campo codigo nao pode ser nulo ou vazio."
+	 * 
+	 * @param id e o id que remete a determinada atividade.
+	 * @return e retornado um int indicando a quantidade de itens PENDENTE.
+	 */
 	public int contaItensPendentes(String id) {
 		if (!existeAtividade(id))
 			throw new IllegalArgumentException("Atividade nao encontrada");
@@ -59,6 +160,18 @@ public class ControladorAtividade {
 		return this.atividades.get(id).getItensPendentes();
 	}
 
+	/**
+	 * Retorna a quantidade itens que possui o status de REALIZADO de
+	 * determinada atividade.
+	 * 
+	 * Caso o id nao remeta a nenhuma atividade sera lancado um IllegalArgumentException:
+	 * "Atividade nao encontrada"
+	 * Caso o id seja nulo ou vazio sera lancado um IllegalArgumentException:
+	 * "Campo codigo nao pode ser nulo ou vazio."
+	 * 
+	 * @param id e o id que remete a determinada atividade.
+	 * @return e retornado um int indicando a quantidade de itens REALIZADO.
+	 */
 	public int contaItensRealizados(String id) {
 		if (!existeAtividade(id))
 			throw new IllegalArgumentException("Atividade nao encontrada");
