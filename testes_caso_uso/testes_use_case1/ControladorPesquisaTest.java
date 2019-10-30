471304a39117a76b71f4e2aea738242547b84bfb
Eniedson Junior <eniedson.silva@gmail.com>
2019-10-30 11:29:47 -0300

    Parte dos testes da us1 terminados

--- testes_caso_uso/testes_use_case1/ControladorPesquisaTest.java
+++ testes_caso_uso/testes_use_case1/ControladorPesquisaTest.java
@@ -2,33 +2,141 @@ package testes_use_case1;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import psquiza.controladores.Sistema;
 
 class ControladorPesquisaTest {
 
-	@Test
-	void testControladorPesquisa() {
-		fail("Not yet implemented");
-	}
+	private Sistema sistema;
 
-	@Test
-	void testGeraCodigo() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testExistePesquisa() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testValidaCampoDeInteresse() {
-		fail("Not yet implemented");
+	@BeforeEach
+	void preparaSistema() {
+		sistema = new Sistema();
+		sistema.cadastraPesquisa("Super computadores resolvendo problemas humanos.", "computadores, IA");
 	}
 
 	@Test
 	void testCadastraPesquisa() {
-		fail("Not yet implemented");
+		sistema.cadastraPesquisa("Pesquisando os animais terrestres", "biologia, animais");
+		assertEquals("BIO1 - Pesquisando os animais terrestres - biologia, animais", sistema.exibePesquisa("BIO1"));
+	}
+	
+	@Test
+	void testCadastraPesquisaMesmosInteresses() {
+		sistema.cadastraPesquisa("computadores legais", "computador");
+		assertEquals("COM2 - computadores legais - computador", sistema.exibePesquisa("BIO1"));
+	}
+
+	@Test
+	void testValidacaoUmInteresse() {
+		sistema.cadastraPesquisa("Pesquisando os animais terrestres", "biologia");
+		assertEquals("BIO1 - Pesquisando os animais terrestres - biologia", sistema.exibePesquisa("BIO1"));
+	}
+
+	@Test
+	void testValidacaoQuatroInteresse() {
+		sistema.cadastraPesquisa("Pesquisando os animais terrestres", "biologia, ciencia, animais, alimentacao");
+		assertEquals("BIO1 - Pesquisando os animais terrestres - biologia, ciencia, animais, alimentacao",
+				sistema.exibePesquisa("BIO1"));
+	}
+
+	@Test
+	void testValidacaoInteresseTresCaracteres() {
+		sistema.cadastraPesquisa("Pesquisando os animais terrestres", "bio, ciencia, animais, alimentacao");
+		assertEquals("BIO1 - Pesquisando os animais terrestres - biologia, ciencia, animais, alimentacao",
+				sistema.exibePesquisa("BIO1"));
+	}
+
+	@Test
+	void testValidacaoCamposDeInteressesGrandeLimite() {
+		sistema.cadastraPesquisa("Pesquisando os animais terrestres",
+				"biobiobiobiobiobiobbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
+		assertEquals("BIO1 - Pesquisando os animais terrestres - biologia, ciencia, animais, alimentacao",
+				sistema.exibePesquisa("BIO1"));
+	}
+
+	@Test
+	void testCadastraPesquisaDescricaoVazia() {
+		try {
+			sistema.cadastraPesquisa("", "biologia");
+			fail("Deve ser lancada uma excessao.");
+		} catch (IllegalArgumentException e) {
+		}
+	}
+
+	@Test
+	void testCadastraPesquisaDescricaoNula() {
+		try {
+			sistema.cadastraPesquisa(null, "biologia");
+			fail("Deve ser lancada uma excessao.");
+		} catch (IllegalArgumentException e) {
+		}
+	}
+
+	@Test
+	void testCadastraPesquisaCamposInteresseVazios() {
+		try {
+			sistema.cadastraPesquisa("Pesquisando os animais terrestres", "");
+			fail("Deve ser lancada uma excessao.");
+		} catch (IllegalArgumentException e) {
+		}
+	}
+
+	@Test
+	void testCadastraPesquisaCamposInteresseNulo() {
+		try {
+			sistema.cadastraPesquisa("Pesquisando os animais terrestres", null);
+			fail("Deve ser lancada uma excessao.");
+		} catch (IllegalArgumentException e) {
+		}
+	}
+
+	@Test
+	void testCValidacaoInteresseVazioInicio() {
+		try {
+			sistema.cadastraPesquisa("Pesquisando os animais terrestres", " ,biologia");
+			fail("Deve ser lancada uma excessao.");
+		} catch (IllegalArgumentException e) {
+		}
+	}
+
+	@Test
+	void testValidacaoInteresseVazioMeio() {
+		try {
+			sistema.cadastraPesquisa("Pesquisando os animais terrestres", "biologia, , animais");
+			fail("Deve ser lancada uma excessao.");
+		} catch (IllegalArgumentException e) {
+		}
+	}
+
+	@Test
+	void testValidacaoInteresseVazioFim() {
+		try {
+			sistema.cadastraPesquisa("Pesquisando os animais terrestres", "biologia,  ");
+			fail("Deve ser lancada uma excessao.");
+		} catch (IllegalArgumentException e) {
+		}
+	}
+
+	@Test
+	void testValidacaoInteressePequeno() {
+		try {
+			sistema.cadastraPesquisa("Pesquisando os animais terrestres", "biologia, ab");
+			fail("Deve ser lancada uma excessao.");
+		} catch (IllegalArgumentException e) {
+		}
+	}
+
+	@Test
+	void testValidacaoInteressesGrande() {
+		try {
+			sistema.cadastraPesquisa("Pesquisando os animais terrestres",
+					"biobiobiobiobiobiobbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
+			fail("Deve ser lancada uma excessao.");
+		} catch (IllegalArgumentException e) {
+		}
 	}
 
 	@Test
@@ -37,12 +145,7 @@ class ControladorPesquisaTest {
 	}
 
 	@Test
-	void testEncerraPesquisaStringString() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testEncerraPesquisaString() {
+	void testEncerraPesquisa() {
 		fail("Not yet implemented");
 	}
 
