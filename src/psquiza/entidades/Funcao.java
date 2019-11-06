00c0b8592d9597765951688cc0ec46c01898ab60
reginaLeticia <regina.felipe@ccc.ufcg.edu.br>
2019-11-06 11:25:06 -0300

    Criei interface Função e Alterei PesquisadorAluno

---
+++ src/psquiza/entidades/Funcao.java
@@ -0,0 +1,11 @@
+package psquiza.entidades;
+
+public interface Funcao {
+
+	public String toString();
+	
+	public String getFuncao();
+	
+	public void setAtributo(String atributo, String novo);
+	
+}
--- src/psquiza/entidades/PesquisadorAluno.java
+++ src/psquiza/entidades/PesquisadorAluno.java
@@ -2,13 +2,12 @@ package psquiza.entidades;
 
 import psquiza.Util;
 
-public class PesquisadorAluno extends  Pesquisador {
-	
+public class PesquisadorAluno implements Funcao {
+
 	private int semestre;
 	private String iea;
 
-	public PesquisadorAluno(String nome, String biografia, String email, String foto, String funcao, String iea, int semestre) {
-		super(nome, biografia, email, foto, funcao);
+	public PesquisadorAluno(String iea, int semestre) {
 		this.semestre = semestre;
 		this.iea = iea;
 	}
@@ -17,23 +16,31 @@ public class PesquisadorAluno extends  Pesquisador {
 		return String.format("%so SEMESTRE", semestre);
 	}
 
-	public void setSemestre(int semestre) {
-		this.semestre = semestre;
-	}
-
 	public String getIea() {
 		return iea.replace(".", ",");
 	}
+	
+	@Override
+	public void setAtributo(String atributo, String novo) {
+		
+		switch (atributo) {
+			case "semestre":
+				semestre = Integer.parseInt(novo);
+				break;
+			case "iea":
+				iea = novo;
 
-	public void setIea(String iea) {
-		this.iea = iea;
+		}
+	}
+	
+	@Override
+	public String getFuncao() {
+		return "Aluno";
 	}
 
 	@Override
 	public String toString() {
-		return String.format("%s - %s - %s", super.toString(),getSemestre(),getIea());
+		return String.format("%s - %s",getSemestre(),getIea());
 	}
-	
-	
 
 }
