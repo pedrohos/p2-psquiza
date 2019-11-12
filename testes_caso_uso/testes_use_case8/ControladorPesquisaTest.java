edcedf807c69d5e10e68109daf4e1b64e1f01e5c
Lucian Julio <lucian.julio@ccc.ufcg.edu.br>
2019-11-12 09:52:21 -0300

    Correções de testes

--- testes_caso_uso/testes_use_case8/ControladorPesquisaTest.java
+++ testes_caso_uso/testes_use_case8/ControladorPesquisaTest.java
@@ -12,23 +12,23 @@ class ControladorPesquisaTest {
 	void testBuscaPesquisa() {
 		ControladorPesquisa c = new ControladorPesquisa();
 		c.cadastraPesquisa("Queimadas nos Oceanos.", "GERAL");
-		
-		assertEquals(c.buscaPesquisa("quei"), "GER1 - Queimadas nos Oceanos.");
-		assertEquals(c.buscaPesquisa("Quei"), "GER1 - Queimadas nos Oceanos.");
-		assertEquals(c.buscaPesquisa("QUEI"), "GER1 - Queimadas nos Oceanos.");
-		
-		assertEquals(c.buscaPesquisa("GERA"), "GER1 - GERAL");
-		assertEquals(c.buscaPesquisa("geRa"), "GER1 - GERAL");
-		assertEquals(c.buscaPesquisa("gera"), "GER1 - GERAL");
-		
+
+		assertEquals(c.buscaPesquisa("quei"), "GER1: Queimadas nos Oceanos.");
+		assertEquals(c.buscaPesquisa("Quei"), "GER1: Queimadas nos Oceanos.");
+		assertEquals(c.buscaPesquisa("QUEI"), "GER1: Queimadas nos Oceanos.");
+
+		assertEquals(c.buscaPesquisa("GERA"), "GER1: GERAL");
+		assertEquals(c.buscaPesquisa("geRa"), "GER1: GERAL");
+		assertEquals(c.buscaPesquisa("gera"), "GER1: GERAL");
+
 		c.cadastraPesquisa("Queimadas em icebergs.", "GERAL");
+
+		assertEquals(c.buscaPesquisa("quei"), "GER2: Queimadas em icebergs. | GER1: Queimadas nos Oceanos.");
+		assertEquals(c.buscaPesquisa("Quei"), "GER2: Queimadas em icebergs. | GER1: Queimadas nos Oceanos.");
+		assertEquals(c.buscaPesquisa("QUEI"), "GER2: Queimadas em icebergs. | GER1: Queimadas nos Oceanos.");
 		
-		assertEquals(c.buscaPesquisa("quei"), "GER1 - Queimadas nos Oceanos. | GER2 - Queimadas em icebergs.");
-		assertEquals(c.buscaPesquisa("Quei"), "GER1 - Queimadas nos Oceanos. | GER2 - Queimadas em icebergs.");
-		assertEquals(c.buscaPesquisa("QUEI"), "GER1 - Queimadas nos Oceanos. | GER2 - Queimadas em icebergs.");
-		
-		assertEquals(c.buscaPesquisa("GERA"), "GER1 - GERAL | GER2 - GERAL");
-		assertEquals(c.buscaPesquisa("geRa"), "GER1 - GERAL | GER2 - GERAL");
-		assertEquals(c.buscaPesquisa("gera"), "GER1 - GERAL | GER2 - GERAL");
+		assertEquals(c.buscaPesquisa("GERA"), "GER2: GERAL | GER1: GERAL");
+		assertEquals(c.buscaPesquisa("geRa"), "GER2: GERAL | GER1: GERAL");
+		assertEquals(c.buscaPesquisa("gera"), "GER2: GERAL | GER1: GERAL");
 	}
 }
